3c5baca4aaf3e941f06fa65e0134badc4eca84d5
Prince Adams <princeadams193@gmail.com>
2025-03-02 19:56:46 +0000

    added the login logic

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-import fastapi
+from fastapi import Depends, FastAPI, HTTPException, security
 import schemas as sma
 from sqlalchemy import orm 
 import services as sv
 
 
-app = fastapi.FastAPI()
+app = FastAPI()
 
 @app.post("/api/register-user/")
-async def register_user(user: sma.UserRequest, db: orm.Session = fastapi.Depends(sv.get_db) ):
+async def register_user(user: sma.UserRequest, db: orm.Session = Depends(sv.get_db) ):
     checked_user = await sv.get_user(user, db)
     if checked_user:
-        raise fastapi.HTTPException(status_code=400, detail="A User already exists with that username")
+        raise HTTPException(status_code=400, detail="A User already exists with that username")
     try:
         creating_user = await sv.create_user(user, db)
     except:
-        raise fastapi.HTTPException(status_code=400, detail="You made a bad request")
+        raise HTTPException(status_code=400, detail="You made a bad request")
     
     print(creating_user)
     
     return await sv.create_token(creating_user)
-    
+
+
+@app.post("/api/login")
+async def login(form_data: security.OAuth2PasswordRequestForm = Depends(), db: orm.Session = Depends(sv.get_db)):
+    db_user = await sv.login(form_data.username, form_data.password, db)
+
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    return await sv.create_token(db_user)
 
 
--- services.py
+++ services.py
@@ -4,8 +4,11 @@ from sqlalchemy import orm
 import models
 import passlib.hash as hash
 import jwt
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException, security, Depends
 
 JWT_SECRET = "nsand329324nrlksndlak;asjdoiqw2"
+oauth2schema = security.OAuth2PasswordBearer("/api/login")
 
 def create_db():
     Base.metadata.create_all(engine)
@@ -26,13 +29,19 @@ async def get_user(user: sma.UserRequest, db: orm.Session):
 
 async def create_user(user: sma.UserRequest, db: orm.Session):
     hash_password = hash.bcrypt.hash(user.password)
-    new_user = models.UserModel(
-        username=user.username,
-        hashed_password=hash_password
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    try:
+        
+        new_user = models.UserModel(
+            username=user.username,
+            hashed_password=hash_password
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Couldn't save the data due to {e}")
+        
     return new_user
 
 
@@ -43,3 +52,26 @@ async def create_token(user: models.UserModel):
     token = jwt.encode(user_dict, JWT_SECRET)
     
     return dict(access_token=token, token_type="bearer")
+
+async def login(identifier: str, password: str, db: orm.Session):
+    user = db.query(models.UserModel).filter_by(username=identifier).first()
+    
+    if not user:
+        return False
+    
+    
+    if not user.password_verification(password):
+        return False
+    
+    return user
+
+
+async def get_current_user(db: orm.Session = Depends(get_db), token: str = Depends(oauth2schema)):
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        user = db.query(models.UserModel).get(id=payload["id"])
+    except:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    return sma.UserResponse.model_validate(user)
+    
